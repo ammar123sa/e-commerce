11d24f917201d146438112991e906f4f6d13d1a6
ammar123sa <ammaralalisa@gmail.com>
2025-05-30 11:59:29 +0300

    change in event

--- app/Events/NewCommentEvent.php
+++ app/Events/NewCommentEvent.php
@@ -10,7 +10,7 @@ use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
 use Illuminate\Foundation\Events\Dispatchable;
 use Illuminate\Queue\SerializesModels;
 use App\Models\Comment ;
-use App\http\Resources\CommentResource ;
+use App\Http\Resources\CommentResource ;
 class NewCommentEvent implements ShouldBroadcast
 {
     use Dispatchable, InteractsWithSockets, SerializesModels;
